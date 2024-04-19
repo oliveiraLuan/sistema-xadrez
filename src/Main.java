f4d1269aa98b6016713f4bf45b1d38b95aa79e98
Luan de Oliveira <luan012012@hotmail.com>
2024-04-18 23:07:04 -0300

    feat: Criado projeto com a classe de Posicao do tabuleiro

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import tabuleiro.Posicao;
+
+public class Main {
+    public static void main(String[] args) {
+        Posicao posicao = new Posicao(5,3);
+        System.out.println(posicao);
+    }
+}
