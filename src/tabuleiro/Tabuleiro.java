b517abea9b3a36127bb34343d9b2fa24de176b8e
Luan de Oliveira <luan012012@hotmail.com>
2024-04-19 22:17:54 -0300

    feat: criado classe de tabuleiro e peca

---
+++ src/tabuleiro/Tabuleiro.java
@@ -0,0 +1,29 @@
+package tabuleiro;
+
+public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int[][] pecas;
+
+    public Tabuleiro(int linhas, int colunas){
+        this.linhas = linhas;
+        this.colunas = colunas;
+        pecas = new int[linhas][colunas];
+    }
+
+    public int getLinhas() {
+        return linhas;
+    }
+
+    public void setLinhas(int linhas) {
+        this.linhas = linhas;
+    }
+
+    public int getColunas() {
+        return colunas;
+    }
+
+    public void setColunas(int colunas) {
+        this.colunas = colunas;
+    }
+}
