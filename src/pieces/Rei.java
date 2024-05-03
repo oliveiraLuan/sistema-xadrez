7e0bf0a9a48262d9a94ddc4d38acc9a77150cb32
Luan de Oliveira <luan012012@hotmail.com>
2024-05-02 23:56:13 -0300

    feat: criado classe para Rei

---
+++ src/pieces/Rei.java
@@ -0,0 +1,15 @@
+package pieces;
+
+import tabuleiro.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaXadrez;
+
+public class Rei extends PecaXadrez {
+    public Rei(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    public String toString(){
+        return "♔";
+    }
+}
