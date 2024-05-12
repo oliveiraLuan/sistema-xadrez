fee94f35e25cb1e6eb46f0ed1bb449985fa8ec33
Luan de Oliveira <luan012012@hotmail.com>
2024-05-11 21:28:34 -0300

    feat: realizando movimentos na partida

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,24 @@
 import xadrez.Partida;
+import xadrez.PosicaoXadrez;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Partida partida = new Partida();
-        Interface.imprimirTabuleiro(partida.getPecas());
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            Interface.imprimirTabuleiro(partida.getPecas());
+            System.out.println();
+
+            System.out.print("Origem: ");
+            PosicaoXadrez origem = Interface.lerPosicaoXadrez(scanner);
+            System.out.print("Destino: ");
+            PosicaoXadrez destino = Interface.lerPosicaoXadrez(scanner);
+
+            partida.moverPeca(origem, destino);
+        }
+
     }
 }
